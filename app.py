97aa1af00c57e1edaafbc28c038ffce0ead6e511
Mariana R da Costa <mariana.rebocho.costa@gmail.com>
2026-03-24 15:12:31 +0000

    Corrected app bugs

--- app.py
+++ app.py
@@ -269,9 +269,7 @@ def update():
 #     # app.run()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port = port)
-
+    app.run(host='0.0.0.0', debug=True, port = 5000)
 
 
 
